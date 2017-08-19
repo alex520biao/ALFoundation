bf6e380ca635756be68a81614670b5ea6b678d33
alex520biao <alex520biao@163.com>
2017-08-19 13:33:24 +0800

    数学

--- Pod/Classes/Macros.h
+++ Pod/Classes/Macros.h
@@ -9,7 +9,11 @@
 #ifndef Macros_h
 #define Macros_h
 
-//交换函数: 交换两个数值变量的值 
+//iOS --- 数学函数 Math Functions http://blog.csdn.net/ios_ping/article/details/53694132
+//iOS math.h 常用数学函数 http://blog.csdn.net/gf771115/article/details/7860414
+//iOS math.h 常用数学函数 http://www.jianshu.com/p/8f384e1c1fef
+
+//交换函数: 交换两个数值变量的值
 #define swap(x, y)\
 x = x + y;\
 y = x - y;\
